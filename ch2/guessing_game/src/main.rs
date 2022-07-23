fdb096aae7b82cbbe25977b44db9342681bfd61b
yun <huayun321@gmail.com>
2022-07-23 20:27:22 +0800

    rand

--- ch2/guessing_game/src/main.rs
+++ ch2/guessing_game/src/main.rs
@@ -1,9 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input the guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
