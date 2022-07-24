34115f5f0e15646d26f16b622846a47e3035c03e
yun <huayun321@gmail.com>
2022-07-24 11:46:46 +0800

    character

---
+++ ch3/char/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //占4字节
+    let c = 'z';
+    let z = '张';
+    let cat = '🐱';
+    println!("The value of c is: {}", c);
+
+    println!("The value of z is: {}", z);
+
+    println!("The value of cat is: {}", cat);
+
+}
