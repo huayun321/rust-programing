ca71b0e574b10f5d1b4d66e0f0e919b1afc71036
yun <huayun321@gmail.com>
2022-07-24 11:37:54 +0800

    math

---
+++ ch3/math/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    //加法
+    let sum = 5 + 10;
+    println!("The value of sum is: {}", sum);
+
+    //减法
+    let difference = 95.5 - 4.3;
+    println!("The value of difference is: {}", difference);
+
+    //乘法
+    let product = 4 * 30;
+    println!("The value of product is: {}", product);
+
+    //除法
+    let quotient = 56.7 / 32.2;
+    println!("The value of quotient is: {}", quotient);
+
+    //取余
+    let remainder = 43 % 5;
+    println!("The value of remainder is: {}", remainder);
+}
