c08f42df19d2b4f9fa280051d004cb3a0b28a397
huayun321 <huayun321@gmail.com>
2022-07-25 14:25:42 +0800

    array

---
+++ ch3/array/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    println!("The value of months is: {:?}", months);
+
+    let a = [0, 1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+
+    println!("The value of first is: {}", first);
+    println!("The value of second is: {}", second);
+}
