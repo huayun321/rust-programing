43f330c06f78b795ebe96a6e0157f89e623d9f2c
huayun321 <huayun321@gmail.com>
2022-07-25 16:26:02 +0800

    while loop

---
+++ ch3/control_flow/loops/while_loop/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+    let mut number = 3;
+
+    while number != 0 {
+        println!("The value of number is: {}", number);
+        number = number - 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
