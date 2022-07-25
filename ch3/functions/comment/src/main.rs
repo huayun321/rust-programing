6d0c072b8801c397bab5c13d5e5d6156da02fe8a
huayun321 <huayun321@gmail.com>
2022-07-25 15:10:12 +0800

    comment

---
+++ ch3/functions/comment/src/main.rs
@@ -0,0 +1,12 @@
+// I'm feeling lucky today!
+// I'm feeling lucky today!
+// I'm feeling lucky today!
+// I'm feeling lucky today!
+
+fn main() {
+    // I'm feeling lucky today!
+    let lucky_number = 7;     // I'm feeling lucky today!
+    println!("The value of lucky_number is: {}", lucky_number);
+    // I'm feeling lucky today!
+    // I'm feeling lucky today!
+}
